5c1d241998b6ec91ab7024d5e25f828851b17e56
Adam Crabtree <dude@noderiety.com>
2014-10-17 16:20:23 -0700

    Add jshint to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,9 @@ var gulp = require('gulp')
 	, traceur = require('gulp-traceur')
 	, changed = require('gulp-changed')
 	, sourcemaps = require('gulp-sourcemaps')
-	, rename = require("gulp-rename")
+	, rename = require('gulp-rename')
 	, insert = require('gulp-insert')
+	, jshint = require('gulp-jshint')
   , rc = require('rc')
   , _ = require('lodash')
   , tracuerConfig = _.omit(rc('traceur'), ['config', '_'])
@@ -26,11 +27,17 @@ gulp.task('traceur', function () {
     .pipe(gulp.dest(destDir));
 })
 
+gulp.task('lint', function() {
+  return gulp.src('./src/*.es6.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
 // Watch Files For Changes
 gulp.task('watch', function() {
-    gulp.watch(srcFiles, ['traceur']);
+    gulp.watch(srcFiles, ['traceur', 'lint']);
 });
 
 
 // Default Task
-gulp.task('default', ['traceur', 'watch']);
+gulp.task('default', ['lint', 'traceur']);
